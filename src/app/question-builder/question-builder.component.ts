98fdf5d89c0d19c28634b24d756d0a9a888bc105
Mohibur-CoU-ICT <mohiburrahman5509@gmail.com>
2022-07-29 03:28:07 +0600

    PeriodicElement table removed

--- src/app/question-builder/question-builder.component.ts
+++ src/app/question-builder/question-builder.component.ts
@@ -14,26 +14,6 @@ const FORM_DATA: Form[] = [
   {id: 6, name: "Form 6", created_on: '15-01-2022'}
 ];
 
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-];
-
 @Component({
   selector: 'app-question-builder',
   templateUrl: './question-builder.component.html',
@@ -44,8 +24,6 @@ export class QuestionBuilderComponent implements OnInit {
 
   displayedColumns: string[] = ['id', 'name', 'created_on'];
   dataSource = FORM_DATA;
-  displayedColumns2: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource2 = ELEMENT_DATA;
   
   constructor() { }
 
