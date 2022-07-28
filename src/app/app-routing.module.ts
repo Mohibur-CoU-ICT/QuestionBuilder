0f5cdae918a62a014796519697013ef617fc97fe
Mohibur-CoU-ICT <mohiburrahman5509@gmail.com>
2022-07-28 20:43:48 +0600

    initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+// import ExpandMoreIcon from '@mui/material/';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+  title = 'Angular_first_app';
+}
