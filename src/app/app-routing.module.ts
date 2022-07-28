8a66bba51e25cf6a2c8fdcdf863e084b7ca8123c
Mohibur-CoU-ICT <mohiburrahman5509@gmail.com>
2022-07-28 20:46:16 +0600

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,28 @@
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { QuestionBuilderComponent } from './question-builder/question-builder.component';
+
+const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginFormComponent
+  },
+  {
+    path: 'question-builder',
+    component: QuestionBuilderComponent
+  },
+  {
+    path: '',
+    component: LoginFormComponent
+  },
+  {
+    path: '**',
+    pathMatch: 'full',
+    component: LoginFormComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
