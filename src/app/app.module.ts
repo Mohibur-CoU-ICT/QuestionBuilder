9d1362b30bde5380e6a5f83dba6d99d3fe141372
Mohibur-CoU-ICT <mohiburrahman5509@gmail.com>
2022-07-28 20:46:07 +0600

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { MatIconModule } from '@angular/material/icon';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card'
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { QuestionBuilderComponent } from './question-builder/question-builder.component';
+import { FormTableComponent } from './form-table/form-table.component';
+import { MatTableModule } from "@angular/material/table";
 
 @NgModule({
   declarations: [
     AppComponent,
+    LoginFormComponent,
+    QuestionBuilderComponent,
+    FormTableComponent
   ],
   imports: [
     BrowserModule,
