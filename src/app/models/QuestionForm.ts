afcb8b87419b35dbbd48f179c166583dee548854
Mohibur-CoU-ICT <mohiburrahman5509@gmail.com>
2022-10-12 00:30:26 +0600

    Update QuestionForm.ts

--- src/app/models/QuestionForm.ts
+++ src/app/models/QuestionForm.ts
@@ -2,5 +2,11 @@ export interface QuestionForm {
   name: string;
   type: string;
   active: boolean;
-  options: string[]
+  options: string[];
+  lowerRange: number;
+  upperRange: number;
+  leftLabel: string;
+  rightLabel: string;
+  allowedFileTypes: string[];
+  maximumFileSize: string;
 };
