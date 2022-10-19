edf62443ec8e891c747855c71e59535757af2cd6
Mohibur-CoU-ICT <mohiburrahman5509@gmail.com>
2022-10-20 01:03:56 +0600

    Form interface modified
    description, updated_on, questions fields added

--- src/app/form-table/form-table.component.ts
+++ src/app/form-table/form-table.component.ts
@@ -4,12 +4,12 @@ import { PeriodicElement } from '../models/PeriodicElement';
 
 
 const FORM_DATA: Form[] = [
-  { id: 1, name: "Form 1", created_on: '10-01-2022' },
-  { id: 2, name: "Form 2", created_on: '11-01-2022' },
-  { id: 3, name: "Form 3", created_on: '12-01-2022' },
-  { id: 4, name: "Form 4", created_on: '13-01-2022' },
-  { id: 5, name: "Form 5", created_on: '14-01-2022' },
-  { id: 6, name: "Form 6", created_on: '15-01-2022' }
+  { id: 1, name: "Form 1", description: '', created_on: '10-01-2022', updated_on: '', questions: [] },
+  { id: 2, name: "Form 2", description: '', created_on: '11-01-2022', updated_on: '', questions: [] },
+  { id: 3, name: "Form 3", description: '', created_on: '12-01-2022', updated_on: '', questions: [] },
+  { id: 4, name: "Form 4", description: '', created_on: '13-01-2022', updated_on: '', questions: [] },
+  { id: 5, name: "Form 5", description: '', created_on: '14-01-2022', updated_on: '', questions: [] },
+  { id: 6, name: "Form 6", description: '', created_on: '15-01-2022', updated_on: '', questions: [] }
 ];
 
 const ELEMENT_DATA: PeriodicElement[] = [
--- src/app/models/Form.ts
+++ src/app/models/Form.ts
@@ -1,5 +1,10 @@
+import { QuestionForm } from "./QuestionForm";
+
 export interface Form {
   id: number;
   name: string;
   created_on: string;
+  updated_on: string;
+  description: string;
+  questions: QuestionForm[];
 };
